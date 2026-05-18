218cce23aa9e61352dd13f318ec9bf5c6c0c570c
vainpirate <shubhamlathi3@gmail.com>
2026-05-18 09:15:17 +0530

    some bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import VoiceAssistant from './transcribe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
     <App />
-    {/* <VoiceAssistant/> */}
   </>
 )
